114f1ac8d89f08dc6baaf50a1919393eec4131b7
MisterBigOriginal <amiralistar20@gmail.com>
2023-05-08 18:11:16 +0430

    Implement dark mode

---
+++ src/components/ColorModeSwitch.tsx
@@ -0,0 +1,13 @@
+import { HStack, Switch, Text, useColorMode } from "@chakra-ui/react";
+
+const ColorModeSwitch = () => {
+  const { toggleColorMode, colorMode } = useColorMode();
+  return (
+    <HStack justifyContent="space-between">
+      <Switch isChecked={colorMode === "dark"} onChange={toggleColorMode} />
+      <Text as="b">Dark Mode</Text>
+    </HStack>
+  );
+};
+
+export default ColorModeSwitch;
--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,12 +1,14 @@
 import { HStack, Heading, Text, ColorModeScript } from "@chakra-ui/react";
+import ColorModeSwitch from "./ColorModeSwitch";
 
 const Navigation = () => {
   return (
-    <HStack>
+    <HStack justifyContent="space-between" padding="10px">
       <Heading as="h1" size="md">
         Open AI - Simple API
       </Heading>
       <Text>Built with Love for Iauet</Text>
+      <ColorModeSwitch />
     </HStack>
   );
 };
