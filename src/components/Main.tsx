d2d7b7411dc120e128158e9a76df67edd64d23b7
MisterBigOriginal <amiralistar20@gmail.com>
2023-05-08 20:04:52 +0430

    Implementing every needed things

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,15 +1,36 @@
-import { Button, Textarea } from "@chakra-ui/react";
+import { useState } from "react";
+import { Flex, Spinner, Text, Button, Textarea } from "@chakra-ui/react";
 import apiHandler from "../services/api-client";
 
 const Main = () => {
+  const [value, setValue] = useState("");
+  const [answer, setAnswer] = useState("");
+  const [showSpinner, setShowSpinner] = useState(false);
+
+  const handleInputChange = (e: { target: { value: string } }) => {
+    const inputValue = e.target.value;
+    setValue(inputValue);
+  };
   const onButtonPressed = () => {
-    apiHandler();
+    setShowSpinner(true);
+    apiHandler(value).then((str: string) => {
+      setAnswer(str);
+      setShowSpinner(false);
+    });
   };
   return (
-    <>
-      <Textarea size="md" placeholder="Here is a sample placeholder" />
+    <Flex p="10px" flexDirection="column">
+      <Text>Matni ke mikhayd tarjome she ro vared konid (Be farsi)</Text>
+      <Textarea
+        value={value}
+        onChange={handleInputChange}
+        size="md"
+        placeholder="After typing press Submit Question"
+      />
       <Button onClick={onButtonPressed}>Submit Question</Button>
-    </>
+      {showSpinner && <Spinner />}
+      {answer.length > 0 && <Textarea value={answer}></Textarea>}
+    </Flex>
   );
 };
 
--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -3,27 +3,29 @@
 export default axios.create({
     baseURL: 'https://api.openai.com/v1', 
     headers: {
-        'Authorization': 'Bearer $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'
+        'Authorization': 'Bearer $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
     }
 })*/
 
 import { Configuration, OpenAIApi } from 'openai';
 
 const configuration = new Configuration({
-    apiKey: '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'
+    apiKey: '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'
 });
 
 const openai = new OpenAIApi(configuration);
 
-export default async function sendRequest() {
+export default async function sendRequest(value : string) {
     const response = await openai.createCompletion({
         model: "text-davinci-003",
-        prompt: "Translate this into Persian:\n\nHow are you\n\n1.",
+        prompt: value,
         temperature: 0.3,
         max_tokens: 100,
         top_p: 1.0,
         frequency_penalty: 0.0,
         presence_penalty: 0.0,
       });
-      console.log(response.data.choices[0].text);
+      
+      
+      return response.data.choices[0].text || "Sorry";
 }
