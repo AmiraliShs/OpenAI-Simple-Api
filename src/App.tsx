1d37afc91cc59667ab40243157a44e4f6d9d82d4
MisterBigOriginal <amiralistar20@gmail.com>
2023-05-08 18:06:34 +0430

    Implement Chakra Theming

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
 import { Grid, GridItem } from "@chakra-ui/react";
+import Navigation from "./components/Navigation";
 
 function App() {
   return (
     <Grid templateAreas={`"nav nav" "main main"`}>
-      <GridItem area="nav" bg="red">
-        Nav
+      <GridItem area="nav">
+        <Navigation />
       </GridItem>
-      <GridItem area="main" bg="coral">
-        Main
-      </GridItem>
+      <GridItem area="main">Main</GridItem>
     </Grid>
   );
 }
---
+++ src/components/Navigation.tsx
@@ -0,0 +1,14 @@
+import { HStack, Heading, Text, ColorModeScript } from "@chakra-ui/react";
+
+const Navigation = () => {
+  return (
+    <HStack>
+      <Heading as="h1" size="md">
+        Open AI - Simple API
+      </Heading>
+      <Text>Built with Love for Iauet</Text>
+    </HStack>
+  );
+};
+
+export default Navigation;
